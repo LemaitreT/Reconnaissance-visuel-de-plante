2cb724c6f9dd3a26e4a6b67a040d2099de94f3e2
CedricDurand <cedricdurand13@sfr.fr>
2019-10-17 12:29:14 +0200

    xmlToJson réalisé

---
+++ xmlToJson.py
@@ -0,0 +1,24 @@
+import json
+import glob
+import re
+import xmltodict
+import os
+
+def doConvertion(path):
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith('.xml'):
+            openFile = open(path+file, "r");
+            nameFile = file.split('.')[0];
+            contenu = openFile.read();
+            monJson = xmltodict.parse(contenu)
+            fichierSrc = open(path+nameFile+".json", "a");
+            fichierSrc.write(json.dumps(monJson));
+            fichierSrc.close();
+            openFile.close();
+
+
+chemin = os.getcwd()+"/train/";
+print('Penser à renseigner votre chemin vers les fichiers XML dans le script ! :)');
+
+doConvertion(chemin);
